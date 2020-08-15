a16c2d4a3ae7a82e49d593a025fabd4e523947a2
almeald <almenaldana@gmail.com>
2020-08-14 19:00:04 -0500

    agregando las rutas carta y cocina

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import './global.scss'
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import HomeContainer from './containers/HomeContainer'
+import Carta from '../src/components/Carta'
+import Cocina from '../src/components/Cocina'
+
+//manejar rutas layout, container
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hola mundo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path='/' exact component={HomeContainer}></Route>
+      <Route path='/carta' component={Carta}></Route>
+      <Route path='/cocina' component={Cocina}></Route>
+    </Router>
   );
 }
 
