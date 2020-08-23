cd255dcdbea0e8e9e631f892eb198a7368fb9e3c
almeald <almenaldana@gmail.com>
2020-08-23 01:41:09 -0500

    agregando array de datos

---
+++ src/db.js
@@ -0,0 +1,109 @@
+
+export default [
+  {
+    'id': '01',
+    'precio': '5',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,h_50,w_72/v1597771249/burger%20queen/cup-coffee-3478127_640_xz58uj.png',
+    'type': 'desayuno',
+    'name': 'Café americano',
+  },
+  {
+    'id': '02',
+    'precio': '7',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,h_56,w_56/v1597771249/burger%20queen/cafe_con_leche_xupcfv.png',
+    'type': 'desayuno',
+    'name': 'Café con leche',
+  },
+  {
+    'id': '03',
+    'precio': '10',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,w_65/v1597771249/burger%20queen/sandwich_airf6l.png',
+    'type': 'desayuno',
+    'name': 'Sandwich de jamón y queso',
+  },
+  {
+    'id': '04',
+    'precio': '7',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,h_58,w_43/v1597771249/burger%20queen/jugo_tqyj8e.png',
+    'type': 'desayuno',
+    'name': 'Jugo de frutas natural',
+  },
+  {
+    'id': '05',
+    'precio': '10',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,h_42,w_48/v1597771249/burger%20queen/burger_simple_ficiis.png',
+    'type': 'menu',
+    'name': 'Hamburguesa simple',
+    'tipoCarne': ['carne', 'pollo', 'vegetariana']
+
+  },
+  {
+    'id': '06',
+    'precio': '15',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,w_48/v1597771249/burger%20queen/burger_doble_opmqur.png',
+    'type': 'menu',
+    'name': 'Hamburguesa doble',
+    'tipoCarne':
+    {
+      'carne': 'carne',
+      'pollo': 'pollo',
+      'vegetariana': 'vegetariana'
+    }
+  },
+  {
+    'id': '07',
+    'precio': '5',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,w_85/v1597708858/burger%20queen/papas_gll8jr.png',
+    'type': 'menu',
+    'name': 'Papas fritas',
+  },
+  {
+    'id': '08',
+    'precio': '7',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,h_53,w_46/v1597771250/burger%20queen/cebolla_ctqvtb.png',
+    'type': 'menu',
+    'name': 'Aros de cebolla',
+  },
+  {
+    'id': '09',
+    'precio': '1',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,w_60/v1597771249/burger%20queen/huevo_k4knf7.png',
+    'type': 'extras',
+    'name': 'Huevo',
+  },
+  {
+    'id': '10',
+    'precio': '1',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,w_60/v1597771249/burger%20queen/queso_uekouh.png',
+    'type': 'extras',
+    'name': 'Queso',
+  },
+  {
+    'id': '11',
+    'precio': '5',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,w_26/v1597777981/burger%20queen/istockphoto-1160833668-170667a-removebg-preview_tud9it.png',
+    'type': 'bebidas',
+    'name': 'Agua 500ml',
+  },
+  {
+    'id': '12',
+    'precio': '7',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,w_26/v1597777981/burger%20queen/istockphoto-1160833668-170667a-removebg-preview_tud9it.png',
+    'type': 'bebidas',
+    'name': 'Agua 750ml',
+  },
+  {
+    'id': '13',
+    'precio': '7',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,h_60,w_35/v1597771249/burger%20queen/gaseosa_gxljfv.png',
+    'type': 'bebidas',
+    'name': 'Gaseosa 500ml',
+  },
+  {
+    'id': '14',
+    'precio': '10',
+    'image': 'https://res.cloudinary.com/dkezzbqbp/image/upload/c_scale,h_60,w_35/v1597771249/burger%20queen/gaseosa_gxljfv.png',
+    'type': 'bebidas',
+    'name': 'Gaseosa 750ml',
+  }
+]
