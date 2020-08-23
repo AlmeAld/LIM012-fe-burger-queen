ed4dab194b9afed739aa67690f16324da776befb
almeald <almenaldana@gmail.com>
2020-08-23 02:03:58 -0500

    layout con nav de cocina

---
+++ src/containers/CocinaContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/layout/Layout'
+// import PendingOrders from '../components/PendingOrders'
+import './CocinaContainer.scss'
+
+function Cocinacontainer() {
+  return(
+    <Layout title= 'Cocina'>
+      <h1>En esta cocina pasan cosas Mágicas</h1>
+      <div>
+        {/* <PendingOrders/> */}
+      </div>
+    </Layout>
+  )
+}
+
+export default Cocinacontainer
