c6d6a6f5cb9c11c16ef9b2909f976c11407a4b92
almeald <almenaldana@gmail.com>
2020-08-25 19:43:21 -0500

    creando estado y funciones para incrementar y decrementar la cantidad de productos seleccionados

--- src/components/Comanda.jsx
+++ src/components/Comanda.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Comanda.scss'
 import Button from './Button'
+import { firebase } from '../../src/configFirebase'
 
 
+function Comanda({ data }) {
+  // console.log({ data })
+  //funcion con firebase para enviar los datos capturados
+  function sendOrder(e) {
+    e.preventDefault()
+    console.log('click enviar pedido')
+    firebase.firestore().collection("orders").add({
+      id: '01',
+      name: 'jugo de frutas',
+      count: '2',
+      time: '10min'
+    })
+  }
 
-function Comanda() {
   return (
     <div className='comanda'>
       <h1 className='comanda-title'>Comanda</h1>
@@ -15,10 +28,22 @@ function Comanda() {
         <p className=' comanda-subtitle-precio'>Precio</p>
       </div>
       <div className='comanda-content-pedido'>
+        {data ? data.map(({ count, name, precio }) =>
+          <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+
+            {count ? <p>{count}</p> : null}
+            <p>{name}</p>
+            <p>S/. {precio}</p>
+          </div>) :
+          <span>No existe</span>
+        }
+      </div>
+      <div>
+        <p>total <span>S/. </span> </p>
       </div>
       <div className='comanda-button'>
         <Button noIcon className='sendOrder'>Cancelar</Button>
-        <Button noIcon className='sendOrder'>Enviar Pedido</Button>
+        <Button noIcon className='sendOrder' onClick={sendOrder}>Enviar Pedido</Button>
       </div>
     </div>
   )
