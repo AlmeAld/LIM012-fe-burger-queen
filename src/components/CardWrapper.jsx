c8176895b6478944d6104948aaa455ae2d811ba7
almeald <almenaldana@gmail.com>
2020-08-25 19:41:31 -0500

    pasando las funciones desde cartaConteiner como props para usarlas en cardProduct

--- src/components/CardWrapper.jsx
+++ src/components/CardWrapper.jsx
@@ -3,21 +3,18 @@ import products from '../db.js'
 import './CardWrapper.scss'
 import CardProduct from './CardProduct'
 
-function CardWrapper({ typeMenu }) {
+function CardWrapper({ title, data, addItemToCart, addCount, decrementCount }) {
 
-  const filterProducts = products.filter((item) => {
-    return item.type === typeMenu
-  })
-
-  const listProducts = filterProducts.map((item) =>
-    <CardProduct key={item.id} {...item} />
-  );
 
   return (
     <div className='menu-card-wrapper'>
-      <h1 className='menu-card-title'>{typeMenu}</h1>
+      <h1 className='menu-card-title'>{title}</h1>
       <ul className="menu-card-list">
-        {listProducts}
+        {
+          data.map(id =>
+            <CardProduct addItemToCart={addItemToCart} addCount={addCount} decrementCount={decrementCount} key={id} {...products.food[id]} />
+          )
+        }
       </ul>
     </div>
   )
