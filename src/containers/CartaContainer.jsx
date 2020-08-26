e61a36f7b2d20d5f7a590700365757a203f3f804
almeald <almenaldana@gmail.com>
2020-08-25 19:45:48 -0500

    creando estado y funciones para incrementar y decrementar la cantidad de productos seleccionados

--- src/containers/CartaContainer.jsx
+++ src/containers/CartaContainer.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import products from '../db.js'
 import './CartaContainer.scss'
 import Layout from '../components/layout/Layout'
 import CardWrapper from '../components/CardWrapper'
@@ -8,16 +9,74 @@ import Comanda from '../components/Comanda'
 
 function CartaContainer() {
 
+  const [state, setState] = useState({
+    categories: products.categories,
+    foods: products.food,
+    selectedFoods: [],
+    total: 0
+  })
+
+
+  //
+  function addItemToCart(idItem) {
+    let flatArrayIds = Object.entries(state.categories).map(([key, value]) => value.list).flatMap(x => x)
+
+
+    let arrFoods = flatArrayIds.map(id => state.foods[id])
+    let selectedItem = arrFoods.find((item) => item.id === idItem);
+
+    let existItem = state.selectedFoods.find((item) => item.id === idItem)
+    // si existe => incrementar el count del item en 1
+    // si no existe => agregar el  item a selectedFoods
+    if (existItem) {
+      setState({ ...state, categories: { ...state.categories } })
+    } else {
+      selectedItem.count = 1
+      setState({ ...state, selectedFoods: [...state.selectedFoods, selectedItem] })
+    }
+  }
+
+  function addCount(idItem) {
+    let flatArrayIds = Object.entries(state.categories).map(([key, value]) => value.list).flatMap(x => x)
+
+
+    let arrFoods = flatArrayIds.map(id => state.foods[id])
+    let selectedItem = arrFoods.find((item) => item.id === idItem);
+    selectedItem.count += 1;
+    setState({ ...state, categories: { ...state.categories } })
+
+  }
+
+  function decrementCount(idItem) {
+    let flatArrayIds = Object.entries(state.categories).map(([key, value]) => value.list).flatMap(x => x)
+
+
+    let arrFoods = flatArrayIds.map(id => state.foods[id])
+    let selectedItem = arrFoods.find((item) => item.id === idItem);
+    if (selectedItem.count === 1) {
+      selectedItem.count = 0
+      let newArr = state.selectedFoods.filter((item) => idItem !== item.id)
+      setState({ ...state, selectedFoods: newArr, categories: { ...state.categories } })
+    } else {
+      selectedItem.count -= 1;
+      setState({ ...state, categories: { ...state.categories } })
+    }
+
+  }
+
+
+
   return (
     <Layout title='Carta'>
       <div className='content-cardWrapper'>
-        <CardWrapper typeMenu='desayuno' />
-        <CardWrapper typeMenu='menu' />
-        <CardWrapper typeMenu='extras' />
-        <CardWrapper typeMenu='bebidas' />
+        {
+          Object.entries(state.categories).map(([key, value]) =>
+            <CardWrapper addItemToCart={addItemToCart} addCount={addCount} decrementCount={decrementCount} key={key} title={value.name} data={value.list} />
+          )
+        }
       </div>
       <div className='container-comanda'>
-        <Comanda> </Comanda>
+        <Comanda data={state.selectedFoods} />
       </div>
 
     </Layout>
