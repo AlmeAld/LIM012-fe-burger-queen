d5e66caea09863bd7d45c8034b7a77b18d931042
almeald <almenaldana@gmail.com>
2020-08-25 19:40:10 -0500

    pasando funciones desde CartaContainer para darle funcionalidad al buton + -

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,34 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from './Button';
 import lessIcon from '../images/less-icon.svg'
 import plusIcon from '../images/plus-icon.svg'
 import './CardProduct.scss';
 
-function CardProduct({ name, image }) {
-  const [activeProduct, setActiveProduct] = useState(false);
-
-  function handleActiveProduct() {
-    setActiveProduct(!activeProduct)
+function CardProduct({ id, name, count, image, addItemToCart, addCount, decrementCount }) {
+  function remove() {
+    console.log('remover')
   }
+
+  function decrement(id) {
+    decrementCount(id)
+  }
+
+  function increment(id) {
+    // console.log('incrementar', id)
+    addItemToCart(id)
+  }
+
 
   return (
     <li className='card'>
       <img src={image} alt="" className='card-image' />
       <p className='card-name'>{name}</p>
       {
-        activeProduct ?
+        count ?
           <>
-            <Button noIcon className='add' onclick={handleActiveProduct}>
+            <Button noIcon className='add' onClick={remove}>
               Remover
               </Button>
             <div className='card-count'>
-              <Button className='less-button' noIcon>
+              <Button className='less-button' noIcon onClick={() => decrement(id)}>
                 <span className='card-count-less-icon'>
                   <img src={lessIcon} alt="" />
                 </span>
               </Button>
-              <p className='card-count-value' >1</p>
-              <Button className='more-button' noIcon>
+              <p className='card-count-value' >{count}</p>
+              <Button className='more-button' noIcon onClick={() => addCount(id)}>
                 <span className='card-count-more-icon'>
                   <img src={plusIcon} alt="" />
                 </span>
@@ -36,7 +44,8 @@ function CardProduct({ name, image }) {
             </div>
           </>
           :
-          <Button noIcon className='add' onclick={handleActiveProduct}>
+          // <Button noIcon className='add' onClick={increment}>
+          <Button noIcon className='add' onClick={() => increment(id)}>
             Agregar
           </Button>
       }
