01ac0e963fa0cb229750582c14697fe6394554ed
almeald <almenaldana@gmail.com>
2020-09-24 16:43:06 -0500

    corrigiendo indentacion

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,13 @@
-import React from 'react'
-import './Button.scss'
+import React from "react";
+import "./Button.scss";
 
 function Button({ icon, noIcon, children, ...rest }) {
-
-
   return (
-    <button {...rest} >
-      {
-        noIcon
-          ? null
-          : <img className='icon-button' src={icon} alt="imagen" />
-      }
+    <button {...rest}>
+      {noIcon ? null : <img className="icon-button" src={icon} alt="imagen" />}
       {children}
     </button>
-  )
+  );
 }
 
-
-export default Button
-
-
+export default Button;
