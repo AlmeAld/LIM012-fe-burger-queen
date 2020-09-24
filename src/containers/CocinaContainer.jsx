40f7f894cd08485396400e7245f1bec27cc18937
almeald <almenaldana@gmail.com>
2020-09-24 16:58:16 -0500

    agregando componentes pendingOrders y OrdersServed

--- src/containers/CocinaContainer.jsx
+++ src/containers/CocinaContainer.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import Layout from '../components/layout/Layout'
-// import PendingOrders from '../components/PendingOrders'
-import './CocinaContainer.scss'
+import React from "react";
+import Layout from "../components/layout/Layout";
+import cutlery from "../images/cutlery.svg";
+import PendingOrders from "../components/PendingOrders";
+import OrdersServed from "../components/OrdersServed";
+import "./CocinaContainer.scss";
 
 function Cocinacontainer() {
-  return(
-    <Layout title= 'Cocina'>
-      <h1>En esta cocina pasan cosas Mágicas</h1>
-      <div>
-        {/* <PendingOrders/> */}
-      </div>
+  return (
+    <Layout title="Cocina">
+      <header className="content-header">
+        <img src={cutlery} alt="icono de cubiertos" />
+        <p className="content-header-title">
+          En esta cocina pasan cosas Mágicas
+        </p>
+        <img src={cutlery} alt="icono de cubiertos" />
+      </header>
+      <main className="content-orders">
+        <PendingOrders />
+        <OrdersServed />
+      </main>
     </Layout>
-  )
+  );
 }
 
-export default Cocinacontainer
+export default Cocinacontainer;
