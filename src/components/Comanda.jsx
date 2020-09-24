114fe7a3ecab7d5ddb4b3a15240d5692369893e5
almeald <almenaldana@gmail.com>
2020-09-24 16:49:09 -0500

    evento que guarda los pedidos en firestore

--- src/components/Comanda.jsx
+++ src/components/Comanda.jsx
@@ -1,67 +1,102 @@
-import React, { useState } from 'react'
-import './Comanda.scss'
-import Button from './Button'
-// import { firebase } from '../../src/configFirebase'
-
+import React, { useState } from "react";
+import "./Comanda.scss";
+import Button from "./Button";
+import { firebase } from "../../src/configFirebase";
 
 function Comanda({ data }) {
-  console.log({ data })
-  const [name, setName] = useState('')
-  const [mesa, setMesa] = useState('')
+  // console.log(data);
+  // data.forEach(ele=>console.log(ele.name) )
+  const [name, setName] = useState("");
+  const [mesa, setMesa] = useState("");
 
   function handleUserName(e) {
-    setName(e.target.value)
+    setName(e.target.value);
   }
   function handleUserMesa(e) {
-    setMesa(e.target.value)
+    setMesa(e.target.value);
   }
 
+  function sumaTotal() {
+    let cuentaTotal = 0;
+    data.forEach((element) => (cuentaTotal += parseInt(element.precioTotal)));
+    return cuentaTotal;
+  }
+
+  const time = new Date();
 
   //funcion con firebase para enviar los datos capturados
   function sendOrder(e) {
-    e.preventDefault()
-    console.log('click enviar pedido', name, mesa)
-    // firebase.firestore().collection("orders").add({
-    //   id: '01',
-    //   name: 'jugo de frutas',
-    //   count: '2',
-    //   time: '10min'
-    // })
+    e.preventDefault();
+
+    const order = data.map((ele) => ({
+      id: ele.id,
+      nameProduct: ele.name,
+      quantity: ele.count,
+    }));
+
+    firebase.firestore().collection("orders").add({
+      order,
+      username: name,
+      usermesa: mesa,
+      initDate: time,
+      endDate: "",
+      status: "pending",
+    });
   }
-  //{props.summary.reduce((acum, item) => acum + item.priceProduct * item.countProduct, 0)}
 
   return (
-    <div className='comanda'>
-      <h1 className='comanda-title'>Comanda</h1>
-      <input type="text" placeholder='Nombre' className='comanda-input-name' onChange={handleUserName} value={name} />
-      <input type="number" placeholder='Mesa' className='comanda-input-mesa' onChange={handleUserMesa} value={mesa} />
-      <div className='comanda-subtitle'>
-        <p className=' comanda-subtitle-detalle'>Detalle de Pedido</p>
-        <p className=' comanda-subtitle-precio'>Precio</p>
+    <div className="comanda">
+      <h1 className="comanda-title">Comanda</h1>
+      <input
+        type="text"
+        placeholder="Nombre"
+        className="comanda-input-name"
+        onChange={handleUserName}
+        value={name}
+      />
+      <input
+        type="number"
+        placeholder="Mesa"
+        className="comanda-input-mesa"
+        onChange={handleUserMesa}
+        value={mesa}
+      />
+      <div className="comanda-subtitle">
+        <p className=" comanda-subtitle-detalle">Detalle de Pedido</p>
+        <p className=" comanda-subtitle-precio">Precio</p>
       </div>
-      <div className='comanda-content-pedido'>
-        <div className='comanda-content-pedido-detalle'>
-          {data ? data.map(({ count, name, precio, total, id }) =>
-            <div key={id} style={{ display: 'flex', justifyContent: 'space-between' }}>
-
-              {count ? <p>{count}</p> : null}
-              <p>{name}</p>
-              <p>S/. {precio}.00</p>
-            </div>) :
+      <div className="comanda-content-pedido">
+        <div className="comanda-content-pedido-detalle">
+          {data ? (
+            data.map(({ count, name, precioTotal, id }) => (
+              <div
+                key={id}
+                style={{ display: "flex", justifyContent: "space-between" }}
+              >
+                {count ? <p>{count}</p> : null}
+                <p>{name}</p>
+                <p>S/. {precioTotal}.00</p>
+              </div>
+            ))
+          ) : (
             <span>No existe</span>
-          }
+          )}
         </div>
       </div>
-      <div className='comanda-total'>
-        <p className='comanda-total-text'>total</p>
-        <span className='comanda-total-count'>S/. 0 </span>
+      <div className="comanda-total">
+        <p className="comanda-total-text">total</p>
+        <span className="comanda-total-count">S/. {sumaTotal()} </span>
       </div>
-      <div className='comanda-button'>
-        <Button noIcon className='sendOrder'>Cancelar</Button>
-        <Button noIcon className='sendOrder' onClick={sendOrder}>Enviar Pedido</Button>
+      <div className="comanda-button">
+        <Button noIcon className="sendOrder">
+          Cancelar
+        </Button>
+        <Button noIcon className="sendOrder" onClick={sendOrder}>
+          Enviar Pedido
+        </Button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Comanda
+export default Comanda;
