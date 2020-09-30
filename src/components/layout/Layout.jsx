656cc6759afc6dfc9c420cdf3b9fa353e065295b
almeald <almenaldana@gmail.com>
2020-09-30 03:33:24 -0500

    traslade el componente nav

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import Nav from "../Nav";
+// import Nav from "../Nav";
 import "./Layout.scss";
+import { Link } from "react-router-dom";
+import { TertiaryButton } from "../Button";
+import homeIcon from "../../images/home.svg";
+import fuenteIcon from "../../images/fuente-icon.svg";
+import searchIcon from "../../images/search-icon.svg";
 
-function Layout({ children, title, className }) {
+function Layout({ children, title, className, setIsOpen }) {
   return (
     <div className="layout">
-      <Nav title={title} />
+      <nav className="nav">
+        <h1 className="title">{title}</h1>
+        <form action="">
+          <input type="text" />
+          <img src={searchIcon} alt="" />
+        </form>
+        <Link to="/">
+          <img src={homeIcon} alt="" />
+        </Link>
+        <TertiaryButton type="button" onClick={() => setIsOpen(true)}>
+          <img src={fuenteIcon} alt="" />
+        </TertiaryButton>
+        {/* <div className='notification'>
+        <p>2</p>
+      </div> */}
+      </nav>
       <div className={className}>{children}</div>
     </div>
   );
