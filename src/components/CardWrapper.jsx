96e75a1153ad17143f403da4682246d92e5460a6
almeald <almenaldana@gmail.com>
2020-09-30 03:26:16 -0500

    indentando codigo

--- src/components/CardWrapper.jsx
+++ src/components/CardWrapper.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import products from '../db.js'
-import './CardWrapper.scss'
-import CardProduct from './CardProduct'
+import React from "react";
+import products from "../db.js";
+import "./CardWrapper.scss";
+import CardProduct from "./CardProduct";
 
-function CardWrapper({ title, data, addItemToCart, addCount, decrementCount, removeCount }) {
-
-
+function CardWrapper({
+  title,
+  data,
+  addItemToCart,
+  addCount,
+  decrementCount,
+  removeCount,
+}) {
   return (
-    <div className='menu-card-wrapper'>
-      <h1 className='menu-card-title'>{title}</h1>
+    <div className="menu-card-wrapper">
+      <h1 className="menu-card-title">{title}</h1>
       <ul className="menu-card-list">
-        {
-          data.map(id =>
-            <CardProduct addItemToCart={addItemToCart} addCount={addCount} decrementCount={decrementCount} removeCount={removeCount} key={id} {...products.food[id]} />
-          )
-        }
+        {data.map((id) => (
+          <CardProduct
+            addItemToCart={addItemToCart}
+            addCount={addCount}
+            decrementCount={decrementCount}
+            removeCount={removeCount}
+            key={id}
+            {...products.food[id]}
+          />
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default CardWrapper
+export default CardWrapper;
 
 // onclick = {() => setProduct([console.log(el.name, el.precio)])}
