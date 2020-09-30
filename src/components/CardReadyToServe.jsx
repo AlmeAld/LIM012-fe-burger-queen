fac2c222d41820dbc9a60876d5e0ba6cdbcb481d
almeald <almenaldana@gmail.com>
2020-09-30 03:38:17 -0500

    card de los pedidos a servir

---
+++ src/components/CardReadyToServe.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./cardReadyToServe.scss";
+
+function CardReadyToServe({ name, mesa, order, endDate }) {
+  const time = new Date();
+
+  function pendingTime(endDate, date) {
+    const currentTime = date.getTime();
+    const endHour = endDate.toDate().getTime();
+    const minutes = Math.floor(((currentTime - endHour) / (1000 * 60)) % 60);
+    const hour = Math.floor(((currentTime - endHour) / (1000 * 60 * 60)) % 24);
+    return `${hour}hrs ${minutes}min`;
+  }
+
+  return (
+    <div className="card-container">
+      <div className="card-container-header">
+        <p>
+          <strong>{name}</strong>
+        </p>
+        <p>
+          <strong>Mesa: {mesa}</strong>
+        </p>
+        <p>
+          <strong>Hace: {pendingTime(endDate, time)}</strong>
+        </p>
+        <input type="button" value="entregado" />
+      </div>
+      <div className="card-container-main">
+        {order.map((list) => (
+          <div key={list.id} className="card-container-main-list">
+            <span>{list.quantity}</span>
+            <span>{list.nameProduct}</span>
+            <input type="checkbox" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CardReadyToServe;
