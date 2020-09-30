461b75a0dba2445ff28f941dbd1197c5e5a212df
almeald <almenaldana@gmail.com>
2020-09-30 03:34:42 -0500

    importando firebase auth

--- src/configFirebase.js
+++ src/configFirebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAOQaRSk9f7EAM3X83hoMpfB5YxsgUsM7g",
@@ -10,9 +10,8 @@ const firebaseConfig = {
   storageBucket: "burger-queen-a8771.appspot.com",
   messagingSenderId: "588456145562",
   appId: "1:588456145562:web:f41359a35210ce76eb2523",
-  measurementId: "G-MN0X9KJB9J"
+  measurementId: "G-MN0X9KJB9J",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-export { firebase }
+export { firebase };
