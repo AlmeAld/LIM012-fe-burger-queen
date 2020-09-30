34754ce754264f20ebab0e8afa44b87919f724cd
almeald <almenaldana@gmail.com>
2020-09-30 03:19:58 -0500

    creando tipos de button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,64 @@
 import React from "react";
 import "./Button.scss";
 
-function Button({ icon, noIcon, children, ...rest }) {
+function Button({ icon, noIcon, children, className, ...rest }) {
+  // para reutilizar className
+  const classNames = ["button", className].join(" ");
+  // console.log(classNames);
+
   return (
-    <button {...rest}>
-      {noIcon ? null : <img className="icon-button" src={icon} alt="imagen" />}
+    <button {...rest} className={classNames}>
       {children}
     </button>
   );
 }
 
-export default Button;
+const PrimaryButton = ({ children, ...rest }) => {
+  return (
+    <Button {...rest} className="primary-btn">
+      {children}
+    </Button>
+  );
+};
+
+const PrimaryButtonIcon = ({ icon, children }) => {
+  return (
+    <Button className="primaryIcon-btn">
+      {icon ? <img className="icon-button" src={icon} alt="imagen" /> : null}
+      {children}
+    </Button>
+  );
+};
+const PrimaryButtonOrder = ({ children, ...rest }) => {
+  return (
+    <Button {...rest} className="primaryOrder-btn">
+      {children}
+    </Button>
+  );
+};
+
+const SecondaryButton = ({ children }) => {
+  return (
+    <Button disabled className="secondary-btn">
+      {children}
+    </Button>
+  );
+};
+
+const TertiaryButton = ({ children, ...rest }) => {
+  return (
+    <Button {...rest} className="tertiary-btn">
+      {children}
+    </Button>
+  );
+};
+
+// export default Button;
+export {
+  PrimaryButton,
+  SecondaryButton,
+  PrimaryButtonIcon,
+  TertiaryButton,
+  PrimaryButtonOrder,
+  Button,
+};
