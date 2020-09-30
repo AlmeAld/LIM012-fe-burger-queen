751410080531af2c648c6e18ca6e1c8a6a8e88c6
almeald <almenaldana@gmail.com>
2020-09-30 03:22:35 -0500

    reemplazando button por el componente del tipo del boton

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,56 +1,56 @@
-import React from 'react'
-import Button from './Button';
-import './CardProduct.scss';
-import lessIcon from '../images/less-icon.svg'
-import plusIcon from '../images/plus-icon.svg'
+import React from "react";
+import { PrimaryButton, TertiaryButton } from "./Button";
+import "./CardProduct.scss";
+import lessIcon from "../images/less-icon.svg";
+import plusIcon from "../images/plus-icon.svg";
 
-function CardProduct({ id, name, count, image, addItemToCart, addCount, decrementCount, removeCount }) {
+function CardProduct({
+  id,
+  name,
+  count,
+  image,
+  addItemToCart,
+  addCount,
+  decrementCount,
+  removeCount,
+}) {
   function remove(id) {
-    removeCount(id)
+    removeCount(id);
   }
 
   function decrement(id) {
-    decrementCount(id)
+    decrementCount(id);
   }
 
   function increment(id) {
-    addItemToCart(id)
+    addItemToCart(id);
   }
 
-
   return (
-    <li className='card'>
-      <img src={image} alt="" className='card-image' />
-      <p className='card-name'>{name}</p>
-      {
-        count ?
-          <>
-            <Button noIcon className='add' onClick={() => remove(id)}>
-              Remover
-              </Button>
-            <div className='card-count'>
-              <Button className='less-button' noIcon onClick={() => decrement(id)}>
-                {/* <span className='card-count-less-icon'> */}
-                <img src={lessIcon} alt="" className='card-count-less-icon' />
-                {/* </span> */}
-              </Button>
-              <p className='card-count-value' >{count}</p>
-              <Button className='more-button' noIcon onClick={() => addCount(id)}>
-                {/* <span className='card-count-more-icon'> */}
-                <img src={plusIcon} alt="" className='card-count-more-icon' />
-                {/* </span> */}
-              </Button>
-            </div>
-          </>
-          :
-          // <Button noIcon className='add' onClick={increment}>
-          <Button noIcon className='add' onClick={() => increment(id)}>
-            Agregar
-          </Button>
-      }
+    <li className="card">
+      <img src={image} alt="" className="card-image" />
+      <p className="card-name">{name}</p>
+      {count ? (
+        <>
+          <PrimaryButton onClick={() => remove(id)}>Remover</PrimaryButton>
+          <div className="card-count">
+            <TertiaryButton onClick={() => decrement(id)}>
+              <img src={lessIcon} alt="" className="card-count-less-icon" />
+            </TertiaryButton>
+            <p className="card-count-value">{count}</p>
+            <TertiaryButton onClick={() => addCount(id)}>
+              <img src={plusIcon} alt="" className="card-count-more-icon" />
+            </TertiaryButton>
+          </div>
+        </>
+      ) : (
+        // <Button noIcon className='add' onClick={increment}>
+        <PrimaryButton className="add" onClick={() => increment(id)}>
+          Agregar
+        </PrimaryButton>
+      )}
     </li>
-  )
+  );
 }
 
-export default CardProduct
-
+export default CardProduct;
